522842aad51e7c98f4470aa1da94aa7cb43c3a0d
Felix <51097237+f321x@users.noreply.github.com>
2024-02-09 19:49:32 +0000

    week4

--- lightning-scavenger-hunt-f321x/submissions/002.sh
+++ lightning-scavenger-hunt-f321x/submissions/002.sh
@@ -8,3 +8,18 @@ channel_id="2811451232288768"
 # You may comment it out and use your own testing node if you would like, but this line *must* be uncommented when you push to test.
 wd=$(pwd)
 lncli="lncli -n=signet --rpcserver=35.209.148.157:10009 --tlscertpath="$wd"/test/lnd0-tls.cert --macaroonpath="$wd"/test/lnd0-readonly.macaroon "
+
+chaninfo=$($lncli getchaninfo $channel_id)
+
+# get correct node policy (node1_pub or node2_pub)
+node1_pub=$(echo $chaninfo | jq -r '.node1_pub')
+node2_pub=$(echo $chaninfo | jq -r '.node2_pub')
+if [ "$node" == "$node1_pub" ]; then
+	node_policy=$(echo $chaninfo | jq -r '.node1_policy')
+else
+	node_policy=$(echo $chaninfo | jq -r '.node2_policy')
+fi
+
+# get base fee
+base_fee=$(echo $node_policy | jq -r '.fee_base_msat')
+echo $base_fee
